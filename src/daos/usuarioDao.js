03857e943b87fa74422ae525684a2295d774ef02
jeri <jerimonski@gmail.com>
2025-06-05 19:41:14 -0400

    env for gitea

--- src/daos/usuarioDao.js
+++ src/daos/usuarioDao.js
@@ -1,39 +1,39 @@
-const db = require("../database/connection");
+const db = require("../database/connection")
 
 class UsuarioDao {
   async getAll() {
-    const result = await db.query("SELECT * FROM usuario");
-    return result.rows;
+    const result = await db.query("SELECT * FROM usuario")
+    return result.rows
   }
 
   async getById(id) {
-    const result = await db.query("SELECT * FROM usuario WHERE id = $1", [id]);
-    return result.rows[0];
+    const result = await db.query("SELECT * FROM usuario WHERE id = $1", [id])
+    return result.rows[0]
   }
 
   async create(usuario) {
     const query = `
       INSERT INTO usuario (nombre, email, contraseña)
       VALUES ($1, $2, $3)
-      RETURNING *`;
-    const values = [usuario.nombre, usuario.email, usuario.contraseña];
-    const result = await db.query(query, values);
-    return result.rows[0];
+      RETURNING *`
+    const values = [usuario.nombre, usuario.email, usuario.contraseña]
+    const result = await db.query(query, values)
+    return result.rows[0]
   }
 
   async update(id, usuario) {
     const query = `
       UPDATE usuario
       SET nombre = $1, email = $2, contraseña = $3
-      WHERE id = $6 RETURNING *`;
-    const values = [usuario.nombre, usuario.email, usuario.contraseña, id];
-    const result = await db.query(query, values);
-    return result.rows[0];
+      WHERE id = $6 RETURNING *`
+    const values = [usuario.nombre, usuario.email, usuario.contraseña, id]
+    const result = await db.query(query, values)
+    return result.rows[0]
   }
 
   async delete(id) {
-    await db.query("DELETE FROM usuario WHERE id = $1", [id]);
+    await db.query("DELETE FROM usuario WHERE id = $1", [id])
   }
 }
 
-module.exports = new UsuarioDao();
+module.exports = new UsuarioDao()
