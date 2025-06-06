ba8b7ff26b022241cedceab5b969149dc074f2db
jeri <jerimonski@gmail.com>
2025-06-05 20:49:19 -0400

    new Jenkinsfile

--- src/routes/comentarios.js
+++ src/routes/comentarios.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../database");
+const pool = require("../database/connection");
 
 router.get("/", async (req, res) => {
   try {
