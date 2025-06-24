5a6bffe73203cb987a51d702dcb88a2fad610dbf
lacomarcaja <lacomarcaja@mail.com>
2025-06-25 01:15:35 +0200

    Actualizar src/routes/usuarioRoutes.js

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-const express = require("express")
+import express from "express"
 const router = express.Router()
-const usuarioController = require("../controllers/usuarioController")
-=======
-import express from 'express';
-const router = express.Router();
-import usuarioController from '../controllers/usuarioController.js';
->>>>>>> a17cb312113f304b816a1942d7fd8f28b9eff55d
+import usuarioController from "../controllers/usuarioController.js"
 
 router.get("/", usuarioController.getAll)
 router.get("/:id(\\d+)", usuarioController.getById)
@@ -14,9 +8,4 @@ router.post("/", usuarioController.create)
 router.put("/:id", usuarioController.update)
 router.delete("/:id", usuarioController.delete)
 
-<<<<<<< HEAD
-module.exports = router
-=======
-
-export default router;
->>>>>>> a17cb312113f304b816a1942d7fd8f28b9eff55d
+export default router
