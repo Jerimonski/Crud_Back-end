482dd3e36924d64190e784da57c076ab0303c6d2
jeri <jerimonski@gmail.com>
2025-07-06 20:11:57 -0400

    fix:delete

--- Back-END/src/dtos/deporteDto.js
+++
@@ -1,9 +0,0 @@
-class DeporteDto {
-  constructor({ nombre, descripcion, entrenador }) {
-    this.nombre = nombre
-    this.descripcion = descripcion
-    this.entrenador = entrenador
-  }
-}
-
-export default DeporteDto
--- Back-END/src/dtos/usuarioDto.js
+++
@@ -1,9 +0,0 @@
-class UsuarioDto {
-  constructor({ nombre, email, contraseña }) {
-    this.nombre = nombre
-    this.email = email
-    this.contraseña = contraseña
-  }
-}
-
-export default UsuarioDto
