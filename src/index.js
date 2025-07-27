763d110f60c0ab0a3998f27b1c66fcc7e6a7077f
jeri <jerimonski@gmail.com>
2025-07-27 18:58:01 -0400

    fix: codigo repetido

--- src/index.js
+++ src/index.js
@@ -37,25 +37,25 @@ if (process.env.NODE_ENV !== "test") {
 }
 
 const app = express()
-const envFile =
-  process.env.NODE_ENV === "production" ? ".env.production" : ".env.development"
-const result = dotenv.config({ path: envFile, override: true })
+// const envFile =
+//   process.env.NODE_ENV === "production" ? ".env.production" : ".env.development"
+// const result = dotenv.config({ path: envFile, override: true })
 
-if (result.error) {
-  console.warn(`No se pudo cargar el archivo de entorno ${envFile}`)
-} else {
-  console.log(`Archivo de entorno cargado: ${envFile}`)
-  console.log("Variables de entorno cargadas:")
-  console.log({
-    NODE_ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT,
-    DB_USER: process.env.DB_USER,
-    DB_NAME: process.env.DB_NAME,
-    DB_HOST: process.env.DB_HOST,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    CORS_ORIGIN: process.env.CORS_ORIGIN,
-  })
-}
+// if (result.error) {
+//   console.warn(`No se pudo cargar el archivo de entorno ${envFile}`)
+// } else {
+//   console.log(`Archivo de entorno cargado: ${envFile}`)
+//   console.log("Variables de entorno cargadas:")
+//   console.log({
+//     NODE_ENV: process.env.NODE_ENV,
+//     PORT: process.env.PORT,
+//     DB_USER: process.env.DB_USER,
+//     DB_NAME: process.env.DB_NAME,
+//     DB_HOST: process.env.DB_HOST,
+//     DB_PASSWORD: process.env.DB_PASSWORD,
+//     CORS_ORIGIN: process.env.CORS_ORIGIN,
+//   })
+// }
 
 app.use(express.json())
 /*
